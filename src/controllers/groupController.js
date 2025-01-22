3577252d8224d29c210420f8d9fd4dc642e2a016
RG-67 <rittikghoshkolkata@gmail.com>
2025-01-23 00:06:54 +0530

    23rd Jan, 2025

--- src/controllers/groupController.js
+++ src/controllers/groupController.js
@@ -16,10 +16,14 @@ const createGroup = async (req, res) => {
         }
         await db.query('BEGIN');
         const groupId = await db.query("SELECT prefix || middle || suffix AS group_id from where id = $1", [1]);
-        const insertQuery = await db.query("INSERT INTO group_members(group_id, userid) VALUES($1, $2) RETURNING *", [groupId, userid]);
-        
+        const insertGroupMembers = "INSERT INTO group_members(group_id, userid) VALUES($1, $2) ON CONFLICT(group_id, userid) DO NOTHING";
+        const insertGroups = await db.query("INSERT INTO groups(group_id, group_name, group_image, group_imageid) values($1, $2, $3, $4)", [groupId, groupname, imagePath, imageId]);
+        for(const users in userid) {
+            await db.query(insertGroupMembers, [groupId, users]);
+        }
+        await db.query("COMMIT");
     } catch (error) {
-
+        await db.query("ROLLBACK");
     }
 }
 
