93542ea1399ba4a5cbc56e2605c9ce788d956c6a
RG-67 <rittikghoshkolkata@gmail.com>
2025-04-27 21:17:07 +0530

    27th April, 2025

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -28,7 +28,7 @@ const updateUserProfile = async (req, res) => {
         if (checkPhone.rows.length > 0) {
             return res.status(400).json({ status: false, message: 'Phone number already exist', data: {} });
         }
-        const response = await db.query('UPDATE users SET username = $1, email = $2, phone = $3, updated_at = LOCALTIMESTAMP WHERE userid = $4 RETURNING *', 
+        const response = await db.query('UPDATE users SET username = $1, email = $2, phone = $3, updated_at = LOCALTIMESTAMP WHERE userid = $4 RETURNING *',
             [username, email, phone, userid]);
         if (response.rows.length === 0) {
             return res.status(404).json({ status: false, message: 'No users found', data: {} });
@@ -42,15 +42,38 @@ const updateUserProfile = async (req, res) => {
 }
 
 
-const getAllUsers = async(req, res) => {
+const getAllUsers = async (req, res) => {
     try {
         const result = await db.query(`SELECT u.userid, u.username, u.userimage, gm.group_id, g.group_name FROM users u LEFT JOIN group_members gm
         ON u.userid=gm.userid LEFT JOIN groups g ON g.group_id=gm.group_id WHERE u.type=$1`, ['Member']);
+        if (result.rows.length === 0) {
+            return res.status(200).json({ status: false, message: 'Users not found', data: [] });
+        }
+        const groupedUsers = {};
+        result.rows.forEach(user => {
+            if (!groupedUsers[user.userid]) {
+                groupedUsers[user.userid] = {
+                    userid: user.userid,
+                    username: user.username,
+                    userimage: user.userimage,
+                    groups: []
+                };
+            }
 
-        if (result.rows.length > 0) return res.status(200).json({status: true, message: 'Data successfully retrieved', data: result.rows});
-        return res.status(200).json({status: false, message: 'Users not found', data: []});
+            const alreadyExists = groupedUsers[user.userid].groups.some(g => g.group_id === user.group_id);
+
+            if (!alreadyExists && user.group_id) {
+                groupedUsers[user.userid].groups.push({
+                    groupId: user.group_id,
+                    groupName: user.group_name
+                });
+            }
+        });
+
+        const userInMultiGroups = Object.values(groupedUsers);
+        return res.status(200).json({ status: true, message: 'Data successfully retrieved', data: userInMultiGroups });
     } catch (error) {
-        res.status(500).json({status: false, message: 'Internal server error', data: []});
+        res.status(500).json({ status: false, message: 'Internal server error', data: [] });
         console.error(error);
     }
 }
